0003efc42fe203e4bb2ac75bf30e30009ba701fb
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-04-10 15:40:13 +0300

    first button information added

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById("more").addEventListener("click", myFunction);
+
+function myFunction(){
+    document.getElementById("plus").innerHTML = "More information about Elizabeth Bennet or Pride and Prejudice visit here https://en.wikipedia.org/wiki/Elizabeth_Bennet";
+}
+
